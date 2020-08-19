9bf4039915558ae0fa760f59d841375c39b268b1
lucaspontoexe <ltx16@hotmail.com>
2020-08-19 00:12:40 -0300

    Create Shopping Item Component

---
+++ src/components/ShoppingItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="shopping-item">
+    <img src="@/assets/corsa.png" alt="Foto do Corsa não-capotado" />
+    <h1>Corsa Horizon</h1>
+    <p>Dá pra fazer altos drifts com ele</p>
+    <strong class="price">R$ 8000</strong>
+    <button class="add-to-cart">+</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.shopping-item {
+  position: relative;
+  width: 200px;
+  height: 256px;
+  background: #f4f4f4;
+  border-radius: 6px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  // overflow: hidden;
+  //   box-sizing: border-box;
+
+  img {
+    width: 100%;
+    height: 156px;
+    object-fit: cover;
+  }
+
+  h1,
+  p,
+  strong {
+    padding: 0px 14px;
+  }
+
+  h1 {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+  }
+
+  p {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 14px;
+
+    color: #686868;
+  }
+
+  strong {
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 20px;
+    margin-bottom: 6px;
+  }
+}
+
+button {
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  width: 40px;
+  height: 40px;
+  border-radius: 6px;
+  border: 0;
+  background: #94ffab;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="homepage">
     <h1>Lista</h1>
+
+    <ShoppingItem/>
     
     <ul>
       <li><b>nome: </b>descrição (opcional), <b>preço</b></li>
@@ -9,3 +11,11 @@
     </ul>
   </div>
 </template>
+
+<script>
+import ShoppingItem from '@/components/ShoppingItem'
+
+export default {
+  components: {ShoppingItem}
+}
+</script>
