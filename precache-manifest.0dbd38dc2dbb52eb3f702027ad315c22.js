8533a81c353abc1618acfe053ad305fd9b6c9fe0
lucaspontoexe <ltx16@hotmail.com>
2020-08-20 18:21:28 -0300

    Updates

--- precache-manifest.0dbd38dc2dbb52eb3f702027ad315c22.js
+++ precache-manifest.0dbd38dc2dbb52eb3f702027ad315c22.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c67ff75a736790e32b1",
-    "url": "/lojinha/css/app.4d1ba608.css"
+    "revision": "56b146f5383c5549713e",
+    "url": "/lojinha/css/app.e3c415d3.css"
   },
   {
-    "revision": "848ffcc4acbf3f5ea831791915991673",
+    "revision": "89a8ee1c074498cd1a8a712023885ecc",
     "url": "/lojinha/index.html"
   },
   {
@@ -12,12 +12,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lojinha/js/about.3c4d9971.js"
   },
   {
-    "revision": "7c67ff75a736790e32b1",
-    "url": "/lojinha/js/app.5875d93d.js"
+    "revision": "56b146f5383c5549713e",
+    "url": "/lojinha/js/app.8e8454bc.js"
   },
   {
-    "revision": "ddca551191be61935b89",
-    "url": "/lojinha/js/chunk-vendors.dbdf39aa.js"
+    "revision": "288bd11faa8f95424a68",
+    "url": "/lojinha/js/chunk-vendors.dde625c7.js"
   },
   {
     "revision": "2c526b3b9e9454b89edaec66774f77bc",
